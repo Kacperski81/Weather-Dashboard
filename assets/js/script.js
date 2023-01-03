cb30974a29eb9ac1c0968df21800a448863b2d3c
Marcin Kacperski <kacperski81@gmail.com>
2023-01-03 14:37:38 +0000

    added responsivnes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,17 @@ var apiKey = '134a7bca60f2f212f1b9faaf871be508';
 
 function displayCurrentWeather(currentData) {
     currentWeather.append(`
-        <h3>${currentData.name} (${moment.unix(currentData.dt).format('DD/MM/YYYY')})</h3>
-        <div class="flex-container">
-            <div>
-                <p>Temp: ${currentData.main.temp} \u00B0C</p>
-                <p>Wind: ${currentData.wind.speed} KPH</p>
-                <p>Humidity: ${currentData.main.humidity}%</p>
-            </div>
-            <div>
-                <img src=https://openweathermap.org/img/w/${currentData.weather[0].icon}.png>
+        <div class="weather-container">
+            <h3>${currentData.name} (${moment.unix(currentData.dt).format('DD/MM/YYYY')})</h3>
+            <div class="weather-details-container">
+                <div>
+                    <p>Temp: ${currentData.main.temp} \u00B0C</p>
+                    <p>Wind: ${currentData.wind.speed} KPH</p>
+                    <p>Humidity: ${currentData.main.humidity}%</p>
+                </div>
+                <div>
+                    <img src=https://openweathermap.org/img/w/${currentData.weather[0].icon}.png>
+                </div>
             </div>
         </div>
     `);
@@ -22,15 +24,17 @@ function displayCurrentWeather(currentData) {
 
 function displayFutureForecast(fiveDayData) {
     fiveDayForecast.append(`
-        <h3>${moment.unix(fiveDayData.dt).format('DD/MM/YYYY')}</h3>
-        <div class="flex-container">
-            <div>
-                <p>Temp: ${fiveDayData.main.temp} \u00B0C</p>
-                <p>Wind: ${fiveDayData.wind.speed} KPH</p>
-                <p>Humidity: ${fiveDayData.main.humidity}%</p>
-            </div>
-            <div>
-                <img src=https://openweathermap.org/img/w/${fiveDayData.weather[0].icon}.png></p>
+        <div class="weather-container">
+            <h3>${moment.unix(fiveDayData.dt).format('DD/MM/YYYY')}</h3>
+            <div class="weather-details-container">
+                <div>
+                    <p>Temp: ${fiveDayData.main.temp} \u00B0C</p>
+                    <p>Wind: ${fiveDayData.wind.speed} KPH</p>
+                    <p>Humidity: ${fiveDayData.main.humidity}%</p>
+                </div>
+                <div>
+                    <img src=https://openweathermap.org/img/w/${fiveDayData.weather[0].icon}.png></p>
+                </div>
             </div>
         </div>
     `);
@@ -44,10 +48,12 @@ function getData() {
             displayCurrentWeather(currentData);
             return $.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
     }).then(function(fiveDayData) {
-        var noonFiveDayData = fiveDayData.list.filter(data => {
-            return data.dt_txt.split('').slice(11,13).join('') === '12';
+        var newFiveDayData = fiveDayData.list.filter(function(data) {
+            if(moment.unix(data.dt).format('DD') !== moment().format('DD')) {
+                return moment.unix(data.dt).add(1,'days').format('HH') === '09';           
+            }
         });
-        noonFiveDayData.forEach(function(item) {
+        newFiveDayData.forEach(function(item) {
             displayFutureForecast(item);
         });
     });    
